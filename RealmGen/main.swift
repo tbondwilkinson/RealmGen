b2649ad5449b8850650bb4011e7c820ffde2ee7c
tom-sparo <tom@sparolabs.com>
2015-12-05 12:46:52 -0600

    Initial Commit

---
+++ RealmGen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RealmGen
+//
+//  Created by Tom Wilkinson on 12/5/15.
+//  Copyright © 2015 Tom Wilkinson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
