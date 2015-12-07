679117233d76d81bb0ec482259d80bac6da4283a
tom-sparo <tom@sparolabs.com>
2015-12-07 10:56:00 -0600

    Cleanup of git project
    1. Remove DerivedData
    2. Add Cartfile
    3. Modify main.swift/realmGen.sh to have paths
    4. Add .gitignore

--- RealmGen/main.swift
+++ RealmGen/main.swift
@@ -7,6 +7,35 @@
 //
 
 import Foundation
+import RealmSwift
+import SwiftyJSON
 
-print("Hello, World!")
-
+var realmPath: String?
+for var i in 1..<Process.arguments.count {
+    switch Process.arguments[i] {
+    case "-p":
+        i++
+        realmPath = Process.arguments[i]
+    default:
+        break
+    }
+}
+let jsonFile = Process.arguments.last!
+let url = NSURL.fileURLWithPath(jsonFile)
+let data = NSData(contentsOfURL: url)
+let dict = JSON(data: data!).dictionaryObject!
+let modelName = dict["modelName"] as! String
+let objects = dict["objects"] as! [[String: AnyObject]]
+if let realmPath = realmPath {
+    Realm.Configuration.defaultConfiguration.path = NSURL.fileURLWithPath(realmPath).path
+}
+let realm = try! Realm()
+print("***** Writing to \(realm.path) *****")
+try! realm.write() {
+    for objectDict in objects {
+        autoreleasepool {
+            print("Creating \(modelName) with dict: \(objectDict)")
+            realm.dynamicCreate(modelName, value: objectDict)
+        }
+    }
+}
---
+++ RealmGen/realmGen.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+#  realmGen.sh
+#  RealmGen
+#
+#  Created by Tom Wilkinson on 12/5/15.
+#  Copyright © 2015 Tom Wilkinson. All rights reserved.
+
+
+function usage
+{
+    echo "usage: realmGen -d swift_directory -s A.swift B.swift -p realm_path -json file.json"
+}
+
+json=
+reading_swift_files=false
+realm_path="."
+swift_files=()
+while [ "$1" != "" ]; do
+    if [[ "$1" == -* ]]; then
+        reading_swift_files=false
+    fi
+    if [ "$reading_swift_files" = true ]; then
+        swift_files+=("$1")
+    else
+        reading_swift_files=false
+        case $1 in
+            -json )  shift
+                    json=$1
+                    ;;
+            -s | -swift_files )
+                    reading_swift_files=true
+                    ;;
+            -d | -swift_directory )
+                    shift
+                    shopt -s nullglob
+                    files=$(find $1 -name "*swift" -type f)
+                    for f in $files; do
+                        if [[ $f != "./main.swift" ]]; then
+                            swift_files+=("$f")
+                        fi
+                    done
+                    ;;
+            -p | -realm_path )
+                    shift
+                    realm_path=$1
+                    ;;
+            * )     usage
+                    exit 1
+        esac
+    fi
+    shift
+done
+cat ${swift_files[@]} main.swift | swift -F Carthage/Build/Mac - -p $realm_path $json
